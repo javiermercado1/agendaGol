dabdc9bf6403689b171fcb5c4dc7f1278c21f32a
Javier Mercado <javimer.25@gmail.com>
2025-07-21 21:08:03 -0500

    Add userLogin to schemas

--- auth_service/app/schemas.py
+++ auth_service/app/schemas.py
@@ -15,6 +15,10 @@ class UserResponse(UserBase):
     class Config:
         orm_mode = True
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
 class Token(BaseModel):
     access_token: str
     token_type: str

6709b411988b66420b7a8170971513f115a6a60b
Javier Mercado <javimer.25@gmail.com>
2025-07-22 15:29:50 -0500

    add if not exist and healtcheck

--- init_databases.sql
+++ init_databases.sql
@@ -1,9 +1,9 @@
--- Script para crear todas las bases de datos necesarias
-CREATE DATABASE auth_db;
-CREATE DATABASE roles_db;
-CREATE DATABASE fields_db;
-CREATE DATABASE reservations_db;
-CREATE DATABASE dashboard_db;
+-- Script para crear todas las bases de datos necesarias (solo si no existen)
+SELECT 'CREATE DATABASE auth_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth_db')\gexec
+SELECT 'CREATE DATABASE roles_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'roles_db')\gexec
+SELECT 'CREATE DATABASE fields_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fields_db')\gexec
+SELECT 'CREATE DATABASE reservations_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'reservations_db')\gexec
+SELECT 'CREATE DATABASE dashboard_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dashboard_db')\gexec
 
 -- Otorgar permisos al usuario postgres
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO postgres;
